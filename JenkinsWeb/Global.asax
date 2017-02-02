bec4e679d6608e9cbc005e5a7f3c9d84a6a9c296
irfan husni alawi <irfan.husni@gmail.com>
2017-02-02 09:02:46 +0700

    initial commite

---
+++ JenkinsWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsWeb.MvcApplication" Language="C#" %>
---
+++ JenkinsWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsWeb.Startup))]
+namespace JenkinsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
